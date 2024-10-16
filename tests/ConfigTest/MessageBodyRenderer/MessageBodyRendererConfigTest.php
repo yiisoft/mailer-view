a56ccaadbc22011f92cf3c1c1104ad1915d37253
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-10-16 09:32:01 +0300

    Add package configuration (#4)

---
+++ tests/ConfigTest/MessageBodyRenderer/MessageBodyRendererConfigTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Mailer\View\Tests\ConfigTest\MessageBodyRenderer;
+
+use Yiisoft\Aliases\Aliases;
+use Yiisoft\Di\Container;
+use Yiisoft\Di\ContainerConfig;
+use Yiisoft\Mailer\MailerInterface;
+use Yiisoft\Mailer\StubMailer;
+use Yiisoft\Mailer\View\MessageBodyRenderer;
+use Yiisoft\Mailer\View\Tests\ConfigTest\ConfigTestCase;
+use Yiisoft\Mailer\View\ViewMailer;
+
+final class MessageBodyRendererConfigTest extends ConfigTestCase
+{
+
+    public function testBase(): void
+    {
+        $container = new Container(
+            ContainerConfig::create()
+                ->withDefinitions(
+                    $this->getDiConfig() + [
+                        Aliases::class => [
+                            '__construct()' => [
+                                [
+                                    'resources' => __DIR__ . '/resources',
+                                ],
+                            ],
+                        ],
+                    ]
+                )
+        );
+
+        $renderer = $container->get(MessageBodyRenderer::class);
+        $html = $renderer->renderHtml('html-content', ['number' => 42]);
+        $text = $renderer->renderText('text-content', ['number' => 42]);
+
+        $this->assertInstanceOf(MessageBodyRenderer::class, $renderer);
+        $this->assertSame('<p>Number: 42.</p>', $html);
+        $this->assertSame('Number: 42.', $text);
+    }
+}
---
+++ tests/ConfigTest/ViewMailer/resources/mail/text-layout.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @var string $content
+ * @var string $title
+ */
+
+echo $title . "\n";
+echo "===\n";
+echo $content;
