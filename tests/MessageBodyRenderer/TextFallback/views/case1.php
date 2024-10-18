f21062319b20fa7626c5967e71676a6d37b5dc90
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-10-18 10:28:20 +0300

    Remove HTML to text converting (#7)

--- tests/MessageBodyRenderer/TextFallback/views/case1.php
+++
@@ -1,1 +0,0 @@
-HTML <b>view file</b> content <a href="http://yiifresh.com/index.php?r=site%2Freset-password&amp;token=abcdef">http://yiifresh.com/index.php?r=site%2Freset-password&amp;token=abcdef</a>
